5a9998562969ad597c1365be9f34f19eab0bf7ef
rd3v <blackscreen405@yahoo.co.id>
2020-01-09 23:00:26 +0800

    adjust surat tugas print size

--- resources/views/pages/protokoler/surat_tugas/surat_tugas.blade.php
+++ resources/views/pages/protokoler/surat_tugas/surat_tugas.blade.php
@@ -80,10 +80,10 @@
 
   <div class="container-fluid">
 
-    <div class="row" style="margin-top:6em">
+    <div class="row" style="margin-top:7.5em">
       <div class="col-md-12 text-center">
-          <p style="text-decoration:underline;font-size:0.7em;font-weight:bolder;margin-bottom: 0">SURAT TUGAS</p>
-          <p style="font-size:0.7em;">Nomor : {{ strtoupper($SuratTugas->nomor) }}</p>
+          <p style="text-decoration:underline;font-size:0.7em;font-weight:bolder;margin: 0">SURAT TUGAS</p>
+          <p style="font-size:0.7em;margin:0">Nomor : {{ strtoupper($SuratTugas->nomor) }}</p>
       </div>
       <div class="col-md-12">
           <p style="font-weight:normal;padding-left:1em;margin-top:0.5em;font-size: 0.7em">
@@ -99,7 +99,6 @@
     </div>
     <div class="row">
       <span class="col-md-1" style="font-size: 0.7em">Kepada :</span>
-      <span class="col-md-1" style="font-size: 0.7em"></span>
     </div>
 
     <div class="row" style="font-size:0.7em;margin-bottom:0.5em">
@@ -146,8 +145,8 @@
     </div>
 
     <div class="row">
-      <span class="col-md-1" style="font-size: 0.7em">Untuk :</span>
-        <div class="col-md-10" style="padding-left:0;font-size: 0.7em">
+      <span class="col-md-1" style="font-size: 0.6em">Untuk :</span>
+        <div class="col-md-10" style="padding-left:0;font-size: 0.6em">
             <ol>
                 <li>{{ ucfirst($SuratTugas->untuk_maksud) }} {{ $SuratTugas->tanggal_mulai }} s/d {{ $SuratTugas->tanggal_akhir }}</li>
                 <li>Segala Biaya yang timbul sehubungan dengan pelaksanaan Surat Tugas ini dibebankan pada APBD Kota Makassar Tahun Anggaran {{ $SuratTugas->tahun_anggaran }}</li>
@@ -156,26 +155,18 @@
         </div>
     </div>
 
-    <div class="row">
+    <div class="row" style="margin:0">
       <div class="col-md-12">
         
-        <table class="table">
-          <tr>
-            <td>
-<p style="font-size: 0.7em">Demikian Surat Tugas ini dikeluarkan, untuk dilaksanakan dengan penuh rasa tanggung jawab.</p>              
-            </td>
-          </tr>
-        </table>
+<p style="font-size: 0.6em;">Demikian Surat Tugas ini dikeluarkan, untuk dilaksanakan dengan penuh rasa tanggung jawab.</p>              
         
       </div>
     </div>
 
-    <br>
-
-    <div class="row">
+    <div class="row" style="margin:0">
       <div class="col-md-12">
         
-            <table class="table table-borderless" style="font-size: 0.7em">
+            <table class="table table-borderless" style="font-size: 0.6em;margin:0">
               <tr>
                 <td></td>
                 <td width="90px">Dikeluarkan</td>
@@ -193,11 +184,11 @@
             <table class="table table-borderless" style="font-size: 0.7em;font-weight: bolder">
               <tr>
                 <td width="70%"></td>
-                <td><p>{{ ucwords($SuratTugas->jabatan) }}</p></td>
+                <td><p style="margin:0">{{ ucwords($SuratTugas->jabatan) }}</p></td>
               </tr>
               <tr>
                 <td></td>
-                <td><p style="margin-top:50px">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ strtoupper($SuratTugas->nama_yang_bertanda_tangan) }}</p></td>
+                <td><p style="margin-top:50px;margin-bottom: 0">&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{{ strtoupper($SuratTugas->nama_yang_bertanda_tangan) }}</p></td>
               </tr>
             </table>
 
@@ -206,11 +197,11 @@
     </div>
 
 
-    <div class="row" style="font-size: 0.6em">
-      <div class="col-md-12">
+    <div class="row" style="font-size: 0.6em;margin:0">
+      <div class="col-md-12" style="margin:0">
         Tembusan :
       </div>
-      <div class="col-md-12">
+      <div class="col-md-12" style="margin:0">
         <ol>
           <li>Pimpinan DPRD Kota Makassar</li>
           <li>Kabag. Keuangan Sek. DPRD Kota Makassar</li>
@@ -221,8 +212,6 @@
         </ol>
       </div>
     </div>
-
-    <br>
 
   </div>
 
